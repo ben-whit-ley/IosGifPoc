58ce2b6aa3cceb67ec26bcf388b55c579d0fb42e
Ben Whitley <ben@touchlab.co>
2019-02-15 13:53:55 -0500

    Initial Commit

---
+++ GifPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GifPOC
+//
+//  Created by Ben Whitley on 2/15/19.
+//  Copyright © 2019 Ben Whitley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
